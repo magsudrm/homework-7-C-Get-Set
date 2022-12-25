5873897e40b4e80c37ac9afb9b5da3a199f55d26
unknown <magsudrm@code.edu.az>
2022-12-25 21:13:57 +0400

    Project Added

---
+++ Homework 7/Homework 7/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Linq;
+
+namespace Homework_7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            User[] users=new User[0];
+            string option;
+            do
+            {
+                Console.WriteLine("1. User elave et");
+                Console.WriteLine("2. Userlere bax");
+                Console.WriteLine("3. Userler uzre axtaris et");
+                Console.WriteLine("0. Prosesi bitir");
+                option = Console.ReadLine();
+                if (option == "1")
+                {
+                    User newUser=createUser();
+                    users = addUser(ref users, newUser);
+
+                }
+                else if (option == "2")
+                {
+                    lookUsers(users);
+                }
+                else if (option == "3")
+                {
+                    findSearch(users);
+                }
+                else if (option == "0")
+                {
+                    Console.WriteLine("Proses bitdi");
+                }
+
+            } while (option != "0");
+
+        }
+
+        static User createUser()
+        {
+            User newUser = new User(null);
+            do
+            {
+                Console.WriteLine("adini yaz: ");
+                string userNameStr = Console.ReadLine();
+                newUser.UserName = (userNameStr);
+
+            } while (newUser.UserName == null);
+            do
+            {
+                Console.WriteLine("Password yaz: ");
+                string passwordStr = Console.ReadLine();
+                newUser.Password = (passwordStr);
+
+            } while (newUser.Password == null);
+            newUser.DateTime = DateTime.Now.ToString();
+            return newUser;
+        }
+        static User[] addUser(ref User[] arr,User newUser)
+        {
+            User[] arr2 = new User[arr.Length + 1]; 
+            for(int i = 0; i < arr.Length; i++)
+            {
+                arr2[i] = arr[i];
+            }
+            arr2[arr2.Length-1] = newUser;
+            return arr2;
+        }
+        static void findSearch(User[] arr)
+        {
+            Console.Write("Axtaris deyerini daxil edin: ");
+            String search=Console.ReadLine();
+            for(int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i].UserName.Contains(search))
+                {
+                    Console.WriteLine("Username: "+arr[i].UserName);
+                    Console.WriteLine("Yaranma tarixi: "+arr[i].DateTime);
+                }
+            }
+        }
+        static void lookUsers(User[] arr)
+        {
+            for(int i=0;i<arr.Length;i++)     
+            {
+                Console.WriteLine("Username: "+arr[i].UserName);
+                Console.WriteLine("Yaranma tarixi: "+arr[i].DateTime);
+            }
+        }
+    }
+}
---
+++ Homework 7/Homework 7/User.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Homework_7
+{
+    internal class User
+    {
+        public User(string username)
+        {           
+        }
+        public string UserName
+        {
+            get => _userName;
+            set
+            {
+                if(IsUserNameTrue(value))
+                _userName=value;
+            } 
+        }
+
+        public string Password
+        {
+            get => _password;
+            set
+            {
+                if (IsPasswordTrue(value)) 
+                _password = value;
+            }
+        }
+
+        public string DateTime
+        {
+            get => _dateTime;
+            set
+            {
+                    _dateTime = value;
+            }
+        }
+
+        private string _dateTime;
+        private string _userName;
+        private string _password;
+
+        public bool IsUserNameTrue(string username)
+        {
+            if (username.Length >=6 && username.Length <= 25)
+            {
+                return true;
+            }
+            else
+                return false;
+        }
+
+        public bool PasswordLength(string paswoord)
+        {
+            if (paswoord.Length >= 8 && paswoord.Length <= 25)
+            {
+                return true;
+            }
+            return false;
+        }
+        public bool HasDigit(string input1)
+        {
+            for(int i = 0; i < input1.Length; i++)
+            {
+                if (char.IsDigit(input1[i]))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public bool HasUpper(string input2)
+        {
+            for(int i = 0; i < input2.Length; i++)
+            {
+                if (char.IsUpper(input2[i]))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public bool HasLower(string input3)
+        {
+            for(int i=0;i< input3.Length; i++)
+            {
+                if (char.IsLower(input3[i]))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public bool IsPasswordTrue(string input)
+        {
+            if(HasDigit(input) && HasLower(input) && HasUpper(input) && PasswordLength(input))
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
